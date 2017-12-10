81bc1f7e36a1a1b866f95ca13bcd0efc004c1a52
TestiPO <amr.cis@gmail.com>
2017-12-10 19:23:13 +0400

    Initial Commit

---
+++ IOTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOTest
+//
+//  Created by Amr Alkhatib on 12/10/17.
+//  Copyright © 2017 Amr Alkhatib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOTestTests/IOTestTests.m
@@ -0,0 +1,39 @@
+//
+//  IOTestTests.m
+//  IOTestTests
+//
+//  Created by Amr Alkhatib on 12/10/17.
+//  Copyright © 2017 Amr Alkhatib. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IOTestTests : XCTestCase
+
+@end
+
+@implementation IOTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
